69befe2c2899ffc7bf25fa7e6f96b050acbe5409
kumar-360 <kumarshanu360@gmail.com>
2021-03-09 16:09:05 +0530

    Files Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React,{useState} from "react";
+import Cards from "./Cards";
+import "./App.css";
+const App = () => {
+  const [numbers,setNumbers]=useState([]);
+  while(numbers.length!==6){
+    let x=Math.random()*6+1;
+    if(numbers.indexOf(x)===-1){
+      setNumbers([...numbers,x]);
+    }
+  }
+  console.log(numbers)
+  return <div>
+    <Cards/>
+    
+  </div>
+};
 
 export default App;
