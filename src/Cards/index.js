0b6c2143588c279abb751c062379477c7a98d589
kumar-360 <kumarshanu360@gmail.com>
2021-03-09 16:09:36 +0530

    Changes made

---
+++ src/Cards/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./style.css"
+
+const Cards = () => {
+    
+    return (
+        <div className="container">
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+        </div>
+    );
+};
+
+export default Cards;
